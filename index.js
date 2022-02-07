8740032fecd572109f9252f829ad69fec6827a17
nipun <anipun747@gmail.com>
2022-02-07 11:21:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// alert("me");
+console.log("hey");
+console.warn("adff");
+console.error("wdf");
+document.write("sadf");
+var a=5;
+var b=7;
+console.log(a+b);
+var ar=[1,2,3,4,5];
+// console.log(ar);
+for(var i=0;i<ar.length;i++)
+{
+    console.log(ar[i]);
+}
+ar.forEach(function(element)
+{console.log(element)}
+)
